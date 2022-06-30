2cf9fc11ccdaf20aede0d7462a55c8711063fb3e
GenaroBautista <soporte.gadgethouse@gmail.com>
2022-06-30 12:52:26 -0500

    modify app

--- redux-react/src/App.js
+++ redux-react/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import React, {useState} from "react";
+import Field from "./componests/Field";
+import Translate from "./componests/Translate";
+import Lenguages from "./componests/Lenguajes"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+  const [Lenguages,setLenguage] = useState('es');
+  const [text, setText] = useState('');
+
+  return(
+    <div>
+      <Field onChange={setText] />
+      <Lenguages Lenguages={Lenguages} onChange={setLenguage} />
+      <Translate text= {text} Lenguages={Lenguages} />
     </div>
   );
 }
 
-export default App;
